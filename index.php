3dfbd46bc9e0588c5041ca2b24924975c1493d5b
Ryan <ryan.elr@outlook.com>
2018-07-25 14:29:20 +0200

    ex6

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice6 Part1</title>
+  </head>
+  <body>
+    <?php
+    $nom = 'Ryan';
+    echo ('Bienvenue '.$nom.', impose toi !');
+    ?>
+  </body>
+</html>
